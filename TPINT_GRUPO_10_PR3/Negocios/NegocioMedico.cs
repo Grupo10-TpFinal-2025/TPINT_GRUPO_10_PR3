9bdd52707608faf2a1ea24c593eec6e57158d33e
Flor2345 <flor2016.disney@gmail.com>
2025-06-19 17:55:07 -0300

    Creacion clases negocios.

---
+++ TPINT_GRUPO_10_PR3/Negocios/NegocioMedico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Entidades;
+
+namespace Negocios
+{
+    class NegocioMedico
+    {
+        DaoMedico daoMedico = new DaoMedico();
+    }
+}

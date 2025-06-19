d0d8a7c7ee247aa2d7c3748fc0db5e2395e3d831
Manuel Villaverde <manuelevillaverde@gmail.com>
2025-06-19 19:55:02 -0300

    cambiando el login y corrigiendo el data source

--- TPINT_GRUPO_10_PR3/Vistas/Login.aspx.designer.cs
+++ TPINT_GRUPO_10_PR3/Vistas/Login.aspx.designer.cs
@@ -42,12 +42,12 @@ namespace Vistas
         protected global::System.Web.UI.WebControls.TextBox txtContraseña;
 
         /// <summary>
-        /// Control Button1.
+        /// Control btnIngresar.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button btnIngresar;
     }
 }

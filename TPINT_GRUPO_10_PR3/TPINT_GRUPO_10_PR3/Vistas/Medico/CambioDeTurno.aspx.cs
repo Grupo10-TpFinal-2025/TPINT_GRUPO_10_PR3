c76fde62997cb59f73684c6d66b1954ca7513ed0
Flor2345 <flor2016.disney@gmail.com>
2025-06-22 17:12:30 -0300

    merge

--- TPINT_GRUPO_10_PR3/TPINT_GRUPO_10_PR3/Vistas/Medico/CambioDeTurno.aspx.cs
+++ TPINT_GRUPO_10_PR3/TPINT_GRUPO_10_PR3/Vistas/Medico/CambioDeTurno.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Vistas.Medico
+namespace Vistas.Medicos
 {
-    public partial class ListarPacientesSeleccionados : System.Web.UI.Page
+    public partial class CambioDeTurno : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {

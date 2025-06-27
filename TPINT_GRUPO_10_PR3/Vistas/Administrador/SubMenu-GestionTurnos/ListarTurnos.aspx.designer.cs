1a042ddfff38bf07eebcdac2bf4ea5ce2e5d4bd7
AlejoTomasAlbor <alejotomasalbor@gmail.com>
2025-06-27 13:52:46 -0300

    SqlDataSource

--- TPINT_GRUPO_10_PR3/Vistas/Administrador/SubMenu-GestionTurnos/ListarTurnos.aspx.designer.cs
+++ TPINT_GRUPO_10_PR3/Vistas/Administrador/SubMenu-GestionTurnos/ListarTurnos.aspx.designer.cs
@@ -87,13 +87,13 @@ namespace Vistas.Administrador.SubMenu_GestionTurnos
         protected global::System.Web.UI.WebControls.Button btnFiltarTurno;
 
         /// <summary>
-        /// Control lvListarTurnos.
+        /// Control ListViewDeTurnos.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView lvListarTurnos;
+        protected global::System.Web.UI.WebControls.ListView ListViewDeTurnos;
 
         /// <summary>
         /// Control SqlDataSourceTurno.
